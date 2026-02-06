c2ecca1b10223d31010e4bce037b414e2f8da407
Bijendramishra123 <bijendramishra2002@gnail.com>
2026-02-06 17:34:33 +0530

    Lightweight API for deployment

--- backend/main.py
+++ backend/main.py
@@ -2,43 +2,47 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json
 import numpy as np
-from sentence_transformers import SentenceTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from db import get_connection
 from similarity import cosine_similarity
 
 app = FastAPI()
 
-print("Loading embedding model...")
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-
 class SearchRequest(BaseModel):
     query: str
 
+def load_products():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT product_id, product_name FROM products_vectors")
+
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return data
+
+products = load_products()
+texts = [p[1] for p in products]
+
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(texts)
 
 @app.post("/search")
 def search_products(req: SearchRequest):
-    query_vector = model.encode(req.query).tolist()
-
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT product_id, product_name, vector FROM products_vectors")
+    query_vec = vectorizer.transform([req.query]).toarray()[0]
 
     results = []
 
-    for product_id, name, vector_json in cursor.fetchall():
-        vector = json.loads(vector_json)
-        score = cosine_similarity(query_vector, vector)
+    for i, (pid, name) in enumerate(products):
+        product_vec = tfidf_matrix[i].toarray()[0]
+        score = cosine_similarity(query_vec, product_vec)
 
         results.append({
-            "product_id": product_id,
+            "product_id": pid,
             "product_name": name,
             "score": float(score)
         })
 
-    cursor.close()
-    conn.close()
-
     results.sort(key=lambda x: x["score"], reverse=True)
-
     return results[:5]
